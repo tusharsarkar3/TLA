4c3fb0322bb0202d69ae9d3ad1e0f8ecfd6db01d
Nishant Rajadhyaksha <79035403+nishant42491@users.noreply.github.com>
2021-07-22 13:57:06 +0530

    Update analyse.py

--- TLA/Analysis/analyse.py
+++ TLA/Analysis/analyse.py
@@ -2,6 +2,11 @@ from TLA.Analysis.table_1 import analysis_table
 from TLA.Analysis.table_2 import analysis_table2
 
 def analyse_data():
+    """
+    Creates .csv files representing results of sentiment analysis carried for the specified dataset.
+    Input-> Takes no input
+    Outpu_> .csv files.
+    """
     analysis_table()
     analysis_table2()
 
